099f0688c91da7e485473680c6642155f0883bf9
yenna23 <yennasopia@student.telkomuniversity.ac.id>
2022-04-22 20:35:07 +0700

    PushModul09

---
+++ PRAKTIKUM09/Connect.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+</body>
+</html>
